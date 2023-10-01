63d0928a0d2b50d0a2b6c34a0009758483f0480a
Mani Dhaka <32302802+manidhaka@users.noreply.github.com>
2023-10-01 18:14:09 +0000

    conversion Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let scrore = "Mani"
+
+
+// console.log(typeof scrore)
+// console.log(typeof(scrore))
+
+let valueInNumber = Number(scrore);
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33Ab" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Mani"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+/* 1=> true; 0=> false
+"" => false
+"mani" => true
+*/
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
